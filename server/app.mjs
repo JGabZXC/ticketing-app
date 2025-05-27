1f4f2c6d3d777ce51ab6d48a6fa0b214132ba109
JGabZXC <jlopezgab25@gmail.com>
2025-05-27 22:47:13 +0800

    Setting up CORS origin

--- server/app.mjs
+++ server/app.mjs
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://ticketing-app-j94u.onrender.com/:5173"],
+    origin: [
+      "https://ticketing-app-j94u.onrender.com/:5173",
+      "https://ticketing-app-self.vercel.app/",
+    ],
     credentials: true,
   })
 );
